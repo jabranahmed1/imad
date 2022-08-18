4022731b8aabe7f99adce4fde25f14a87000d74a
jabranahmeed1 <jabranahmed1999@gmail.com>
2022-08-18 18:44:08 +0100

    projet hiringapp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'contact', component: ContactComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: "**", component: PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents= [LoginComponent, RegisterComponent, HomeComponent, ContactComponent, BlogComponent, PagenotfoundComponent]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Title } from '@angular/platform-browser'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hiringapp';
+  title = 'HiringApp';
+
+  constructor(private titleService: Title){
+    titleService.setTitle('Hiring App');
+  }
 }
